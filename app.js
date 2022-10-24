2de9db17ba4a851d2b3172564d51ffbf038de877
Viet Nguyen <viet.ng304@gmail.com>
2022-10-24 16:43:24 +0800

    TEST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to ISEC6000 with manual review'));
+app.get('/', (req, res) => res.send('Welcome to ISEC6000 with manual review - test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
